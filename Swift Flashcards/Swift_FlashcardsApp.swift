60327e6fa71162ac3905366ef96e8f30ebabb6e6
Kyle Musco <kmusco@ycp.edu>
2021-06-05 18:50:44 -0400

    Initial Commit

---
+++ Swift Flashcards/Swift_FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_FlashcardsApp.swift
+//  Swift Flashcards
+//
+//  Created by Kyle Musco on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
